be236ff3c469e3a3a818fadc9d614106082cf579
ritalernmont <lermontorita123@gmail.com>
2026-05-22 19:19:25 +0300

    хоспади, помоги с форматом

--- src/updatetransaction.cpp
+++ src/updatetransaction.cpp
@@ -1,6 +1,17 @@
 #include "updatetransaction.hpp"
 #include "indexstore.hpp"
 
+UpdateTransaction::UpdateTransaction(IndexStore& index)
+    : store_(index), backup_(index.get_index()) {}
+
+UpdateTransaction::~UpdateTransaction()
+{
+    if (!committed_)
+    {
+        store_.get_index() = backup_;
+    }
+}
+
 Result<void> UpdateTransaction::add_document(Document doc)
 {
     if (backup_.has_document(doc.id))
