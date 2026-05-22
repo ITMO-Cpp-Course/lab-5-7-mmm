d7823543529683d85ba67f4df379e15776376591
ritalernmont <lermontorita123@gmail.com>
2026-05-22 19:23:11 +0300

    ну плиз

--- src/updatetransaction.hpp
+++ src/updatetransaction.hpp
@@ -19,8 +19,48 @@ class UpdateTransaction
     explicit UpdateTransaction(IndexStore& index) : store_(index), backup_(index.get_index()) {}
 
     ~UpdateTransaction()
+    {
+        if (!committed_)
+        {
+            store_.get_index() = backup_;
+        }
+    }
 
     Result<void> add_document(Document doc)
+    {
+        if (backup_.has_document(doc.id))
+        {
+            return std::unexpected(IndexError::DocumentAlreadyExists);
+        }
+        pending_adds_.push_back(std::move(doc));
+        return {};
+    }
+
     Result<void> remove_document(size_t doc_id)
+    {
+        if (!backup_.has_document(doc_id))
+        {
+            return std::unexpected(IndexError::DocumentNotFound);
+        }
+        pending_removes_.push_back(doc_id);
+        return {};
+    }
+
     Result<void> commit()
+    {
+        for (auto& doc : pending_adds_)
+        {
+            auto res = store_.get_index().add_document(std::move(doc));
+            if (!res)
+                return res;
+        }
+        for (auto doc_id : pending_removes_)
+        {
+            auto res = store_.get_index().remove_document(doc_id);
+            if (!res)
+                return res;
+        }
+        committed_ = true;
+        return {};
+    }
 };
